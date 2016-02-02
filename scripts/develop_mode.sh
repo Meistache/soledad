4d35d6721ca98d91a8c3d380b681c2017b000c9a
Victor Shyba <victor.shyba@gmail.com>
2016-02-02 09:25:58 -0300

    [doc] explain necessary steps after develop_mode.sh
    It's not enough to run the script for development as you need the
    underlying dependencies, such as leap.common.
    This commit explains the options to get them and how to proceed.

--- scripts/develop_mode.sh
+++ scripts/develop_mode.sh
@@ -5,3 +5,9 @@ cd ../client
 python setup.py develop
 cd ../server
 python setup.py develop
+
+echo "All dependencies were installed, except for LEAP libraries"
+echo "In order to develop you will need the latest for each of them"
+echo "On each folder there is a pkg/requirements-leap.pip declaring it."
+echo "You can either clone them or use the ones from leapcode or pixelated."
+echo "example: pip install -r server/pkg/requirements-latest.pip"
