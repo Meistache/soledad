b7867a22350b0acffe6f513c49b3a1395a9158c9
Folker Bernitt <fbernitt@thoughtworks.com>
2016-02-12 14:08:30 +0100

    [feat] Add develop_mode.sh [<leap|pixelated>]
    - Create a workspace for leap or pixelated by e.g. calling:
      ./scripts/develop_mode.sh leap
    - (Re-)add u1db and dirspec dependencies to latest

--- scripts/develop_mode.sh
+++ scripts/develop_mode.sh
@@ -1,13 +1,80 @@
-#!/bin/sh
-cd common
-python setup.py develop
-cd ../client
-python setup.py develop
-cd ../server
-python setup.py develop
+#!/bin/bash
 
-echo "All dependencies were installed, except for LEAP libraries"
-echo "In order to develop you will need the latest for each of them"
-echo "On each folder there is a pkg/requirements-leap.pip declaring it."
-echo "You can either clone them or use the ones from leapcode or pixelated."
-echo "example: pip install -r server/pkg/requirements-latest.pip"
+CHECK_VENV="1"
+
+usage() { echo "Usage: $0 [-n] [<leap|pixelated>]" 1>&2; exit 1; }
+
+function install_requirements_for() {
+  local REQUIREMENTS_MODE=$1
+
+  # upgrade pip and setuptools to the latest version
+  set -e
+  pip install --upgrade pip
+  pip install --upgrade setuptools
+
+  for MODULE in client server common ; do
+    echo "Installing dependencies for ${MODULE}"
+    pushd $MODULE
+    pip install -r "pkg/requirements-${REQUIREMENTS_MODE}.pip"
+    popd
+  done
+
+  # install testing dependendies (only in common)
+  pushd common
+  # install testing dependendies (only in common)
+  pip install -r pkg/requirements-testing.pip
+  popd
+
+  # reinstall scrypt
+  pip uninstall -y scrypt || true
+  pip install scrypt
+
+  echo -e "\n\n"
+  echo "To run the tests execute:"
+  echo "trial leap.soledad.common.tests"
+  echo
+}
+
+while getopts "hn" OPT; do
+	case $OPT in
+		n)
+			CHECK_VENV=
+			;;
+		h)
+			usage
+			exit 0
+			;;
+		*)
+			usage
+			exit 1
+			;;
+	esac
+done
+shift $((OPTIND-1))
+
+if [ -z "$VIRTUAL_ENV" -a -n "$CHECK_VENV" ] ; then
+	echo "You did not activate a python virtualenv"
+	exit 1
+fi
+
+if [ $# -gt 1 ] ; then
+	echo "Unexpected number of arguments: $#"
+	usage
+	exit 1
+fi
+
+
+REQUIREMENTS_MODE=$1
+if [ -n "$REQUIREMENTS_MODE" ] ; then
+	install_requirements_for $REQUIREMENTS_MODE
+else
+	cd common
+	python setup.py develop
+	cd ../client
+	python setup.py develop
+	cd ../server
+	python setup.py develop
+fi
+
+exit 0
+
